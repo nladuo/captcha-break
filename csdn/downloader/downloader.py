9218d5c9f9860c239ea07b1c8ce037dcd8269a34
nladuo <kalen25115@gmail.com>
2016-06-22 14:19:23 +0800

    modified:   csdn/downloader/downloader.py

--- csdn/downloader/downloader.py
+++ csdn/downloader/downloader.py
@@ -7,7 +7,7 @@ import os
 url = "http://download.csdn.net/index.php/rest/tools/validcode/source_ip_validate/10.5711163911089325"
 for i in range(100):
     resp = requests.get(url)
-    filename = "./captchas/" + str(uuid.uuid1()) + ".png"
+    filename = "./captchas/" + str(uuid.uuid4()) + ".png"
     with open(filename, 'wb') as f:
         for chunk in resp.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
